725615922604b77ed97dece4c9d4ab1ae0fee95a
PikaChu5 <phyuphyuthin.pk@gmail.com>
2024-07-14 00:22:49 +0630

    Done work!

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const userinput = document.getElementById("username")
 const submitBtn = document.getElementById("button")
 
+submitBtn.addEventListener("click",function btn() {
+    alert("hello world!")
+})
+    
